4b224e24d47035aeef9cfc51fe86d303edcf1218
JRoshthen1 <jroshthen1@gmail.com>
2021-10-28 13:49:45 +0200

    footer

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-var gradient = new Gradient();
-gradient.initGradient("#canvas");
-
 // Navigation Bar
 //config vars
 const lagAmount = 50;
@@ -26,7 +23,3 @@ const updatePinPosition = (time) => {
 	requestAnimationFrame(updatePinPosition);
 }
 requestAnimationFrame(updatePinPosition);
-
-
-
-//
